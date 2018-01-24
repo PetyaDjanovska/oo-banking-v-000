05d87300398e25dc7be47d20755901eb87819809
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-24 15:46:26 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,9 @@
 class Transfer
   # your code here
 
-  def initialize
-
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
   end
 end
