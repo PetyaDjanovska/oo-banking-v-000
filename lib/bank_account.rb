0ff604fb1acb518fe4906fcaadac4214aa97b862
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-24 15:37:00 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,12 @@
 class BankAccount
+  attr_accessor :balance, :status
   attr_reader :name
 
 
   def initialize(name)
     @name = name
-
+    @balance = 1000.00
+    @status = "open"
   end
 
 end
